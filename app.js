bb730cc09ca83197f39aae7ed4f5cf5db2c61bf7
Sonu <sonu@chapter247.com>
2020-07-02 13:38:47 +0530

    app.js added for rendering

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express"),
+  app = express(),
+  http = require("http"),
+  path = require("path");
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.use("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "dist", "index.html"))
+);
+app.use("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "dist", "index.html"))
+);
+
+const server = http.createServer(app);
+const port = process.env.PORT || 8012;
+
+server
+  .listen(port)
+  .on("listening", () => console.log(`App Started on port ${port}`));
