b57f07a1797f87e8a70dbe3d6988d59bf5c3f74d
Sonu <sonu@chapter247.com>
2020-07-02 13:43:04 +0530

    Added command

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const express = require("express"),
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.use("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "dist", "index.html"))
+  res.sendFile(path.join(__dirname, "build", "index.html"))
 );
 app.use("/*", (req, res) =>
-  res.sendFile(path.join(__dirname, "dist", "index.html"))
+  res.sendFile(path.join(__dirname, "build", "index.html"))
 );
 
 const server = http.createServer(app);
