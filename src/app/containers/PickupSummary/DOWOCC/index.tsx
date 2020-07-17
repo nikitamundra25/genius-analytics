1d2c9908d410ec2b60c4c07ccba39980d3ec12d6
nikita mundra <nikitam.chapter247@gmail.com>
2020-07-17 18:14:06 +0530

    Worked on pickup summary chartData

--- src/app/containers/PickupSummary/DOWOCC/index.tsx
+++ src/app/containers/PickupSummary/DOWOCC/index.tsx
@@ -4,101 +4,58 @@ const MixedCharts = React.lazy(() =>
   import("../../../components/Charts/MixedCharts")
 );
 
-const DOWData = [
-  {
-    name: "Mon",
-    RoomNts: 59,
-    ADR: 100,
-  },
-  {
-    name: "Tue",
-    RoomNts: 69,
-    ADR: 54,
-  },
-  {
-    name: "Wed",
-    RoomNts: 75,
-    ADR: 26,
-  },
-  {
-    name: "Thu",
-    RoomNts: 62,
-    ADR: 56,
-  },
-  {
-    name: "Fri",
-    RoomNts: 76,
-    ADR: 56,
-  },
-  {
-    name: "Sat",
-    RoomNts: 88,
-    ADR: 46,
-  },
-  {
-    name: "Sun",
-    RoomNts: 45,
-    ADR: 60,
-  },
-  {
-    name: "Total",
-    RoomNts: 68,
-    ADR: 59,
-  },
-];
-const Charts = [
-  {
-    dataSource: DOWData,
-    xName: "name",
-    yName: "RoomNts",
-    type: "Column",
-    fill: "#4684bd",
-    name: "Room Nts",
-    width: 1,
-    marker: {
-      dataLabel: {
-        visible: true,
-        position: "Bottom",
-        font: {
-          fontWeight: "600",
-          color: "#fff",
-        },
-      },
-    },
-  },
- 
-  {
-    dataSource: DOWData,
-    xName: "name",
-    yName: "ADR",
-    type: "Line",
-    fill: "#ee792b",
-    name: "ADR",
-    width: 2,
-    dashArray: "5",
-    marker: {
-      visible: true,
-      width: 8,
-      height: 8,
-      fill:"#ee792b",
-      border: { width: 0, color: "#ee792b" },
-      dataLabel: {
-        visible: true,
-        position: "Top",
-        fill:"#ee792b",
-        font: {
-          fontWeight: "600",
-          color: "#ffffff",
-        
-        },
-      },
-    },
-  },
-];
-
 
 const PickupDOWOCCSegment = (props:any) => {
-  const{index} = props;
+  const{index,OccData} = props;
+  const Charts = [
+    {
+      dataSource: OccData,
+      xName: "name",
+      yName: "RoomNts",
+      type: "Column",
+      fill: "#4684bd",
+      name: "Room Nts",
+      width: 1,
+      marker: {
+        dataLabel: {
+          visible: true,
+          position: "Bottom",
+          font: {
+            fontWeight: "600",
+            color: "#fff",
+          },
+        },
+      },
+    },
+   
+    {
+      dataSource: OccData,
+      xName: "name",
+      yName: "ADR",
+      type: "Line",
+      fill: "#ee792b",
+      name: "ADR",
+      width: 2,
+      dashArray: "5",
+      marker: {
+        visible: true,
+        width: 8,
+        height: 8,
+        fill:"#ee792b",
+        border: { width: 0, color: "#ee792b" },
+        dataLabel: {
+          visible: true,
+          position: "Top",
+          fill:"#ee792b",
+          font: {
+            fontWeight: "600",
+            color: "#ffffff",
+          
+          },
+        },
+      },
+    },
+  ];
   return (
     <>
      
--- src/app/containers/PickupSummary/PickupSegment/index.tsx
+++ src/app/containers/PickupSummary/PickupSegment/index.tsx
@@ -4,102 +4,63 @@ const MixedCharts = React.lazy(() =>
   import("../../../components/Charts/MixedCharts")
 );
 
-const PickupData = [
-  {
-    name: "Best Flexible Rate",
-    RoomNts: 8,
-    ADR: 100,
-  },
-  {
-    name: "Leisure Break",
-    RoomNts: 17,
-    ADR: 54,
-  },
-  {
-    name: "Corporate Contracted",
-    RoomNts: 63,
-    ADR: 26,
-  },
-  {
-    name: "Consortia",
-    RoomNts: 38,
-    ADR: 56,
-  },
-  {
-    name: "Promotions",
-    RoomNts: 25,
-    ADR: 56,
-  },
-  {
-    name: "Groups",
-    RoomNts: 18,
-    ADR: 46,
-  },
-  {
-    name: "OTAs",
-    RoomNts: 18,
-    ADR: 60,
-  },
-  {
-    name: "FIT",
-    RoomNts: 12,
-    ADR: 59,
-  },
-];
-const Charts = [
-  {
-    dataSource: PickupData,
-    xName: "name",
-    yName: "RoomNts",
-    type: "Column",
-    fill: "#4684bd",
-    name: "Room Nts",
-    width: 1,
-    marker: {
-      dataLabel: {
-        visible: true,
-        rx: 10,
-        ry: 10,
-        font: {
-          fontWeight: "600",
-          color: "#000",
-        },
-      },
-    },
-  },
- 
-  {
-    dataSource: PickupData,
-    xName: "name",
-    yName: "ADR",
-    type: "Line",
-    fill: "#ee792b",
-    name: "ADR",
-    width: 2,
-    marker: {
-      visible: false,
-      width: 8,
-      height: 8,
-      fill:"#ee792b",
-      border: { width: 0, color: "#ee792b" },
-      dataLabel: {
-        visible: true,
-        position: "Top",
-        fill:"#ee792b",
-        border: { width: 1, color: "#4684bd" },
-        font: {
-          fontWeight: "600",
-          color: "#ffffff",
-        
-        },
-      },
-    },
-  },
-];
+
+
 
 
 const PickupSegment = (props: any) => {
-  const{index} = props;
+  const{index, segmentData} = props;
+  const Charts = [
+    {
+      dataSource: segmentData,
+      xName: "name",
+      yName: "RoomNts",
+      type: "Column",
+      fill: "#4684bd",
+      name: "Room Nts",
+      width: 1,
+      marker: {
+        dataLabel: {
+          visible: true,
+          rx: 10,
+          ry: 10,
+          font: {
+            fontWeight: "600",
+            color: "#000",
+          },
+        },
+      },
+    },
+   
+    {
+      dataSource: segmentData,
+      xName: "name",
+      yName: "ADR",
+      type: "Line",
+      fill: "#ee792b",
+      name: "ADR",
+      width: 2,
+      marker: {
+        visible: false,
+        width: 8,
+        height: 8,
+        fill:"#ee792b",
+        border: { width: 0, color: "#ee792b" },
+        dataLabel: {
+          visible: true,
+          position: "Top",
+          fill:"#ee792b",
+          border: { width: 1, color: "#4684bd" },
+          font: {
+            fontWeight: "600",
+            color: "#ffffff",
+          
+          },
+        },
+      },
+    },
+  ];
+
   return (
     <>
      
