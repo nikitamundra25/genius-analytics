62f5f8fdb11cac7b6c258ee36aacab01c45fc2d8
nikita mundra <nikitam.chapter247@gmail.com>
2020-08-08 17:18:23 +0530

    minor change

--- src/app/containers/PickupSummary/index.tsx
+++ src/app/containers/PickupSummary/index.tsx
@@ -2,7 +2,6 @@ import React, { FunctionComponent, useEffect } from "react";
 import DashboardWidget from "./Widget";
 import { useSelector, useDispatch } from "react-redux";
 import TopBar from "./TopBar";
-import { DashboardLayoutComponent } from "@syncfusion/ej2-react-layouts";
 import { IRootState } from "../../../interfaces";
 import { PickupSummaryRequest } from "../../../actions";
 import Loader from "../../components/Loader/Loader";
@@ -10,9 +9,6 @@ import "./index.scss";
 import { ErrorComponent } from "../../components/Error";
 
 const PickupSummary: FunctionComponent = () => {
-  const cellSpacing = [15, 15];
-  let restoreModel: any = [];
-  let dashboardObj: any;
   const dispatch = useDispatch();
   const PickupReducer = useSelector((state: IRootState) => state.PickupReducer);
 
@@ -21,21 +17,12 @@ const PickupSummary: FunctionComponent = () => {
     // eslint-disable-next-line
   }, []);
 
-  // To reset drag & drop when select date
-  const RestorePanel = () => {
-    dashboardObj.panels = restoreModel;
-  };
-
-  // To store dashboard data initially
-  const created = () => {
-    restoreModel = dashboardObj.serialize();
-  };
 
   const { pickupSummaryList, isPickupLoading, isPickupError } = PickupReducer;
 
   return (
     <>
-      <TopBar handleReset={RestorePanel} />
+      <TopBar />
       <div className='animated fadeIn'>
         {isPickupLoading ? (
           <Loader />
