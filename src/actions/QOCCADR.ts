7d328138b1b97568e43d897b82d107021a0e8055
nikita mundra <nikitam.chapter247@gmail.com>
2020-08-27 20:06:17 +0530

    add actions for dashboard yearly graphs

--- src/actions/QOCCADR.ts
+++ src/actions/QOCCADR.ts
@@ -5,9 +5,31 @@ export enum QOCCADRActionTypes {
   TOGGLE_QOCCADR_LOADER = "Toggle QOCCADR loader",
   QOCCADR_DATA_SUCCESS = "Requested QOCCADR data fetched successfully",
   QOCCADR_DATA_FAILED = "Requested QOCCADR data fetching failed",
+
+  REQUETS_QOCCADR_PAST_DATA = "Request QOCCADR past Data",
+  TOGGLE_QOCCADR_PAST_LOADER = "Toggle QOCCADR past loader",
+  QOCCADR_PAST_DATA_SUCCESS = "Requested QOCCADR past data fetched successfully",
+  QOCCADR_PAST_DATA_FAILED = "Requested QOCCADR past data fetching failed",
+
+  REQUETS_QOCCADR_FUTURE_DATA = "Request QOCCADR future Data",
+  TOGGLE_QOCCADR_FUTURE_LOADER = "Toggle QOCCADR future loader",
+  QOCCADR_FUTURE_DATA_SUCCESS = "Requested QOCCADR future data fetched successfully",
+  QOCCADR_FUTURE_DATA_FAILED = "Requested QOCCADR future data fetching failed",
 }
 
 export const requestQOCCADRData = createAction(QOCCADRActionTypes.REQUETS_QOCCADR_DATA);
 export const toggleQOCCADRLoader = createAction(QOCCADRActionTypes.TOGGLE_QOCCADR_LOADER);
 export const QOCCADRDataSuccess = createAction(QOCCADRActionTypes.QOCCADR_DATA_SUCCESS);
 export const QOCCADRDataFailed = createAction(QOCCADRActionTypes.QOCCADR_DATA_FAILED);
+
+// PAST 
+export const requestQOCCADRPastData = createAction(QOCCADRActionTypes.REQUETS_QOCCADR_PAST_DATA);
+export const toggleQOCCADRPastLoader = createAction(QOCCADRActionTypes.TOGGLE_QOCCADR_PAST_LOADER);
+export const QOCCADRPastDataSuccess = createAction(QOCCADRActionTypes.QOCCADR_PAST_DATA_SUCCESS);
+export const QOCCADRPastDataFailed = createAction(QOCCADRActionTypes.QOCCADR_PAST_DATA_FAILED);
+
+// FUTURE
+export const requestQOCCADRFutureData = createAction(QOCCADRActionTypes.REQUETS_QOCCADR_FUTURE_DATA);
+export const toggleQOCCADRFutureLoader = createAction(QOCCADRActionTypes.TOGGLE_QOCCADR_FUTURE_LOADER);
+export const QOCCADRFutureDataSuccess = createAction(QOCCADRActionTypes.QOCCADR_FUTURE_DATA_SUCCESS);
+export const QOCCADRFutureDataFailed = createAction(QOCCADRActionTypes.QOCCADR_FUTURE_DATA_FAILED);
