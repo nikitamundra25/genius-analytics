c5eec143fa894b29b7f7d35defd11d899f0b08f4
nikitamundra <nikitam.chapter247@gmail.com>
2020-10-19 13:17:08 +0530

    Rollback auth0 integration

--- src/app/containers/PasswordProtection.tsx
+++ src/app/containers/PasswordProtection.tsx
@@ -1,35 +1,60 @@
-import React from "react";
-import { Form, FormGroup,  Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, FormGroup, FormControl, Button } from "react-bootstrap";
+import { useCookies } from "react-cookie";
+import { toast } from "react-toastify";
 import logo from "../../assets/img/logo150.png";
 import video1 from "../../assets/img/video-login.mp4";
-import { useAuth0 } from "@auth0/auth0-react";
+
 const PasswordProtection = () => {
-  const { loginWithRedirect } = useAuth0();
+  const [passcode, setPasscode] = useState("");
+  const [cookies, setCookie] = useCookies(["token"]);
+  const submit = (e: any) => {
+    e.preventDefault();
+    if (passcode ===  process.env.REACT_APP_APPLICATION_PASSCODE) {
+      setCookie("token", cookies.AWSELB, {
+        path: "/",
+        expires: new Date(new Date().getTime() + 3600),
+        maxAge: 3600,
+      });
+      window.location.reload();
+    } else {
+      toast.error("Invalid passcode.");
+    }
+  };
   return (
     <>
-      <div className="app-body overflow-hidden">
-        {/* <video autoPlay playsInline muted src={video} /> */}
-
-        <div className="login-container">
+      <div className='app-body overflow-hidden'>
+      {/* <video autoPlay playsInline muted src={video} /> */}
+     
+        <div className='login-container'>
           <video autoPlay muted loop className="login-video">
             <source src={video1} type="video/mp4" />
           </video>
-          <div className="login-box">
-            <div className="login-logo">
-              <img src={logo} alt="" />
+          <div className='login-box'>
+            <div className='login-logo'>
+                <img
+                  src={logo}
+                  alt=''
+                />
             </div>
 
-            <h3 className="text-center login-title">
-              Click on Login to access the website
+            <h3 className='text-center login-title'>
+              Enter Passcode to access the website
             </h3>
-
-            <Form>
+            
+            <Form onSubmit={submit}>
               <FormGroup>
-                <Button
-                  className="btn btn-primary btn-block"
-                  onClick={() => loginWithRedirect()}
-                >
-                  Log In
+                <FormControl
+                  type='password'
+                  autoComplete='off'
+                  placeholder='Enter passcode'
+                  value={passcode}
+                  onChange={(e: any) => setPasscode(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Button type='submit' block>
+                  Confirm Passcode
                 </Button>
               </FormGroup>
             </Form>
