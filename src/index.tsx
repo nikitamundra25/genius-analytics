8bb27e7a04014c434d55aa5eb4d65cc0a60e0f7a
nikitamundra <nikitam.chapter247@gmail.com>
2020-10-27 15:49:22 +0530

    added redirectCallBack to handle error

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
 import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { auth0Domain, auth0ClientId } from "./config/AppConfig";
+const history = createBrowserHistory();
 
+const onRedirectCallback = async(appState:any) => {
+  console.log("appStateappStateappState",appState);
+  
+  history.push(
+    appState && appState.returnTo
+      ? appState.returnTo
+      : window.location.pathname
+  );
+};
 
-ReactDOM.render( <Auth0Provider
+ReactDOM.render(
+  <Auth0Provider
     domain={auth0Domain}
     clientId={auth0ClientId}
     redirectUri={window.location.origin}
-  > <App />  </Auth0Provider>, document.getElementById("root"));
+    onRedirectCallback={onRedirectCallback}
+  >
+    {" "}
+    <App />{" "}
+  </Auth0Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
