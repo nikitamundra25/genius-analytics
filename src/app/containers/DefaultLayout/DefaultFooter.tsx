fab4114a4b95085f1f45d57b5251d6b75f6a035c
nikitamundra <nikitam.chapter247@gmail.com>
2020-11-11 12:48:58 +0530

    comment footer code

--- src/app/containers/DefaultLayout/DefaultFooter.tsx
+++ src/app/containers/DefaultLayout/DefaultFooter.tsx
@@ -7,9 +7,9 @@ class DefaultFooter extends Component {
       <div className="w-100">
         <Row>
           <Col sm={"6"}><div>2020 © Genius.</div></Col>
-          <Col sm={"6"}>
+          {/* <Col sm={"6"}>
             <div className="text-sm-right ">Design &amp; Developed by Chapter247</div>
-          </Col>
+          </Col> */}
         </Row>
       </div>
       
